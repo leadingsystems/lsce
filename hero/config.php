350807a554cdc578297e5ebca06df2dbcdcfa7a6
Daniel Bitsch <bitsch@leadingsystems.de>
2021-10-05 14:33:05 +0200

    adding LSCE-Hero (initial)

---
+++ hero/config.php
@@ -0,0 +1,17 @@
+<?php
+
+$arr_config = [
+    'label' => ['Hero'],
+    'types' => ['content', 'module'],
+    'contentCategory' => 'LS',
+    'moduleCategory' => 'miscellaneous',
+    'wrapper' => [
+        'type' => 'none'
+    ],
+    'standardFields' => ['cssID'],
+    'fields' => [
+
+
+
+    ]
+];
