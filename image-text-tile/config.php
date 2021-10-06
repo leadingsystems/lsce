37802b466ba9a617e90971f5bbaf220fe7331ab9
Daniel Bitsch <bitsch@leadingsystems.de>
2021-10-06 15:28:12 +0200

    adding DCA-Fields

--- image-text-tile/config.php
+++ image-text-tile/config.php
@@ -20,7 +20,6 @@ $arr_config = [
                 'tl_class' => 'w50'
             ],
         ],
-
         'image' => [
             'label' => ['Bildauswahl'],
             'inputType' => 'fileTree',
@@ -28,39 +27,63 @@ $arr_config = [
                 'fieldType' => 'radio',
                 'filesOnly' => true,
                 'extensions' => Contao\Config::get('validImageTypes'),
-                'tl_class' => 'w50 clr'
+                'tl_class' => 'long clr'
             ]
         ],
-
+        'imageSize' => [
+            'label' => ['Bildbreite und Bildhöhe', ''],
+            'inputType' => 'imageSize',
+            'reference' => &$GLOBALS['TL_LANG']['MSC'],
+            'eval' => [
+                'rgxp' => 'digit',
+                'includeBlankOption' => true,
+                'tl_class' => 'w50'
+            ],
+            'options' => \System::getImageSizes()
+        ],
         'textfield' => [
             'label' => ['Text'],
             'inputType' => 'textarea',
-            'eval'  => array('rte'=>'tinyMCE', 'style' => 'height: 140px', 'tl_class' => 'w100 clr'),
+            'eval'  => [
+                'rte'=>'tinyMCE',
+                'style' => 'height: 200px',
+                'tl_class' => 'clr'
+            ],
         ],
-
         'hyperlinkboxes' => [
             'label' => ['Buttons'],
             'elementLabel' => '%s. Button',
             'inputType' => 'list',
-            'fields' => array(
-                'hyperlinkText' => [
-                    'label' => ['Button-Text'],
-                    'inputType' => 'text',
-                    'eval' => [
-                        'tl_class' => 'w50 clr'
-                    ]
-                ],
-
+            'fields' => [
                 'hyperlinkHref' => [
-                    'label' => ['Link'],
+                    'label' => ['Button-Verlinkung'],
                     'inputType' => 'url',
                     'eval' => [
                         'tl_class' => 'w50'
                     ]
                 ],
-            ),
+                'hyperlinkNewWindow' => [
+                    'label' => $GLOBALS['TL_LANG']['MSC']['target'],
+                    'inputType' => 'checkbox',
+                    'eval' => [
+                        'tl_class' => 'w50 cbx m12'
+                    ]
+                ],
+                'hyperlinkText' => [
+                    'label' => ['Button-Beschriftung'],
+                    'inputType' => 'text',
+                    'eval' => [
+                        'tl_class' => 'w50 clr'
+                    ]
+                ],
+                'hyperlinkTitle' => [
+                    'label' => ['Button-Titel'],
+                    'inputType' => 'text',
+                    'eval' => [
+                        'tl_class' => 'w50'
+                    ]
+                ],
+            ],
         ],
-
-
     ]
 ];
