df7d3fdd4ed5742eebbc6ddbfcdce31087077b1b
Daniel Bitsch <bitsch@leadingsystems.de>
2021-11-19 14:38:10 +0100

    rename infobox -> infobar

---
+++ infobar/config.php
@@ -0,0 +1,56 @@
+<?php
+
+$arr_config = [
+    'label' => ['Infobar'],
+    'types' => ['content', 'module'],
+    'contentCategory' => 'LS',
+    'moduleCategory' => 'miscellaneous',
+    'wrapper' => [
+        'type' => 'none'
+    ],
+    'standardFields' => ['cssID'],
+    'fields' => [
+        'headlineGroup' => [
+            'label' => ['Überschrift'],
+            'inputType' => 'group'
+        ],
+
+        'headline' => [
+            'inputType' => 'standardField'
+        ],
+
+        'textGroup' => [
+            'label' => ['Elementkonfiguation'],
+            'inputType' => 'group'
+        ],
+
+        'mode' => [
+            'label' => ['Darstellungsvariante'],
+            'inputType' => 'select',
+            'options' => [
+                'normal' => 'Normal',
+                'invert' => 'Invertriert'
+            ],
+            'eval' => [
+                'tl_class' => 'w50'
+            ]
+        ],
+
+        'boxes' => [
+            'label' => ['Infoeinträge'],
+            'elementLabel' => '%s. Info',
+            'inputType' => 'list',
+            'fields' => [
+                'text' => [
+                    'label' => ['Inhalt'],
+                    'inputType' => 'textarea',
+                    'eval' => [
+                        'rte' => 'tinyMCE',
+                        'style' => 'height: 220px',
+                        'tl_class' => 'clr'
+                    ]
+                ]
+            ]
+        ]
+    ]
+];
--- infobox/copy/rsce_infobox_config.php
+++
@@ -1,3 +0,0 @@
-<?php
-include('./assets/lsce/infobox/config.php');
-return $arr_config;
