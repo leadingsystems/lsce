82a8fb37decbaebac033aa5568355f50f12a10c3
Daniel Bitsch <bitsch@leadingsystems.de>
2021-11-19 10:40:21 +0100

    initial commit

---
+++ infobox/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$arr_config = [
+    'label' => ['Infobox'],
+    'types' => ['content', 'module'],
+    'contentCategory' => 'LS',
+    'moduleCategory' => 'miscellaneous',
+    'wrapper' => [
+        'type' => 'none'
+    ],
+    'standardFields' => ['cssID'],
+    'fields' => [
+        'headlineGroup' => [
+
+    ]
+];
