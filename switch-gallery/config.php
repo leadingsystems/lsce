68a22c6ef215dbc1449f127989d00a96f9bfa166
Daniel Bitsch <bitsch@leadingsystems.de>
2021-12-06 13:05:42 +0100

    initial commit

---
+++ switch-gallery/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$arr_config = [
+    'label' => ['Switch Gallery'],
+    'types' => ['content', 'module'],
+    'contentCategory' => 'LS',
+    'moduleCategory' => 'miscellaneous',
+    'wrapper' => [
+        'type' => 'none'
+    ],
+    'standardFields' => ['cssID'],
+    'fields' => [
+
+    ]
+];
