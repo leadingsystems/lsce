10deba908b8da715281741fae43c7a64d4134879
Daniel Bitsch <bitsch@leadingsystems.de>
2021-12-07 15:40:25 +0100

    adding config, template and style

--- switch-gallery/config.php
+++ switch-gallery/config.php
@@ -10,6 +10,29 @@ $arr_config = [
     ],
     'standardFields' => ['cssID'],
     'fields' => [
-
+        'image' => [
+            'label' => ['Bilddatei(en)', ''],
+            'inputType' => 'fileTree',
+            'eval' => [
+                'fieldType' => 'checkbox',
+                'multiple' => true,
+                'orderField' => 'orderSRC',
+                'filesOnly' => true,
+                'extensions' => Contao\Config::get('validImageTypes'),
+                'isGallery' => true,
+                'tl_class' => 'w50'
+            ]
+        ],
+        'size' => [
+            'label' => ['Bildbreite und Bildhöhe', ''],
+            'inputType' => 'imageSize',
+            'reference' => &$GLOBALS['TL_LANG']['MSC'],
+            'eval' => [
+                'rgxp' => 'digit',
+                'includeBlankOption' => true,
+                'tl_class' => 'clr w50'
+            ],
+            'options' => \System::getImageSizes()
+        ]
     ]
 ];
