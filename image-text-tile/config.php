6118c1be263aff1cbe891e08a0c2078cb965e33c
Daniel Bitsch <coder@chiron501.de>
2021-12-14 16:17:28 +0100

    Merge pull request #6 lsce: image-text-tile
    * initial commit
    
    * adding DCA-Fields
    
    * adding HTML-Template
    
    * adding style variant 2
    
    * optimize style
    
    * removing shadow, adding button-class
    
    * total rework
    
    * optimize style & template
    
    * optimize style
    
    * optimize style
    
    * fix style variable
    
    * adding link-class

---
+++ image-text-tile/config.php
@@ -0,0 +1,96 @@
+<?php
+$arr_config = [
+    'label' => ['Bild mit Textkachel'],
+    'types' => ['content', 'module'],
+    'contentCategory' => 'LS',
+    'moduleCategory' => 'miscellaneous',
+    'wrapper' => [
+        'type' => 'none'
+    ],
+    'standardFields' => ['cssID'],
+    'fields' => [
+        'orderImageText' => [
+            'label' => ['Kachel Reihenfolge'],
+            'inputType' => 'select',
+            'options' => [
+                'order-text-image' => 'Textkachel links, Bildkachel rechts',
+                'order-image-text' => 'Bildkachel links, Textkachel rechts'
+            ],
+            'eval' => [
+                'tl_class' => 'w50'
+            ],
+        ],
+        'image' => [
+            'label' => ['Bildauswahl'],
+            'inputType' => 'fileTree',
+            'eval' => [
+                'fieldType' => 'radio',
+                'filesOnly' => true,
+                'extensions' => Contao\Config::get('validImageTypes'),
+                'tl_class' => 'long clr'
+            ]
+        ],
+        'imageSize' => [
+            'label' => ['Bildbreite und Bildhöhe', ''],
+            'inputType' => 'imageSize',
+            'reference' => &$GLOBALS['TL_LANG']['MSC'],
+            'eval' => [
+                'rgxp' => 'digit',
+                'includeBlankOption' => true,
+                'tl_class' => 'w50'
+            ],
+            'options' => \System::getImageSizes()
+        ],
+        'textfield' => [
+            'label' => ['Text'],
+            'inputType' => 'textarea',
+            'eval'  => [
+                'rte'=>'tinyMCE',
+                'style' => 'height: 200px',
+                'tl_class' => 'clr'
+            ],
+        ],
+        'hyperlinkBoxes' => [
+            'label' => ['Buttons'],
+            'elementLabel' => '%s. Button',
+            'inputType' => 'list',
+            'fields' => [
+                'hyperlinkHref' => [
+                    'label' => ['Button-Verlinkung'],
+                    'inputType' => 'url',
+                    'eval' => [
+                        'tl_class' => 'w50'
+                    ]
+                ],
+                'hyperlinkNewWindow' => [
+                    'label' => $GLOBALS['TL_LANG']['MSC']['target'],
+                    'inputType' => 'checkbox',
+                    'eval' => [
+                        'tl_class' => 'w50 cbx m12'
+                    ]
+                ],
+                'hyperlinkText' => [
+                    'label' => ['Button-Beschriftung'],
+                    'inputType' => 'text',
+                    'eval' => [
+                        'tl_class' => 'w50 clr'
+                    ]
+                ],
+                'hyperlinkTitle' => [
+                    'label' => ['Button-Titel'],
+                    'inputType' => 'text',
+                    'eval' => [
+                        'tl_class' => 'w50'
+                    ]
+                ],
+                'hyperlinkClass' => [
+                    'label' => ['Button-Klasse'],
+                    'inputType' => 'text',
+                    'eval' => [
+                        'tl_class' => 'w50 clr'
+                    ]
+                ],
+            ],
+        ],
+    ]
+];
