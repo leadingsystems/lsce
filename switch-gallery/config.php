378ae705c4455d1a2b693cf14caeb1f2cffbe454
Daniel Bitsch <coder@chiron501.de>
2021-12-17 10:09:02 +0100

    Merge pull request #9 lsce: switch-gallery
    * initial commit
    
    * adding config, template and style
    
    * fixing image sorting
    
    * optimize _style.scss

---
+++ switch-gallery/config.php
@@ -0,0 +1,42 @@
+<?php
+
+$arr_config = [
+    'label' => ['Switch Gallery'],
+    'types' => ['content', 'module'],
+    'contentCategory' => 'LS',
+    'moduleCategory' => 'miscellaneous',
+    'wrapper' => [
+        'type' => 'none'
+    ],
+    'standardFields' => ['cssID'],
+    'fields' => [
+        'image' => [
+            'label' => ['Bilddatei(en)', ''],
+            'inputType' => 'fileTree',
+            'eval' => [
+                'fieldType' => 'checkbox',
+                'multiple' => true,
+                'orderField' => 'orderSRC',
+                'filesOnly' => true,
+                'extensions' => Contao\Config::get('validImageTypes'),
+                'isGallery' => true,
+                'tl_class' => 'w50'
+            ],
+            'sql' => 'blob NULL',
+            'load_callback' => [
+                ['tl_content', 'setMultiSrcFlags']
+            ]
+        ],
+        'size' => [
+            'label' => ['Bildbreite und Bildhöhe', ''],
+            'inputType' => 'imageSize',
+            'reference' => &$GLOBALS['TL_LANG']['MSC'],
+            'eval' => [
+                'rgxp' => 'digit',
+                'includeBlankOption' => true,
+                'tl_class' => 'clr w50'
+            ],
+            'options' => \System::getImageSizes()
+        ]
+    ]
+];
