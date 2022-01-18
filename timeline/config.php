ae07c7aba5dbe4aaae52bbfdfd1e3a123a2f5353
Daniel Bitsch <bitsch@leadingsystems.de>
2022-01-18 16:16:54 +0100

    initial commit

---
+++ timeline/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$arr_config = [
+    'label' => ['Timeline'],
+    'types' => ['content','module'],
+    'contentCategory' => 'LS',
+    'moduleCategory' => 'miscellaneous',
+    'wrapper' => [
+        'type' => 'none'
+    ],
+    'standardFields' => ['cssID'],
+    'fields' => [
+
+    ]
+];
