12a04927d1e8215a86b44525ef4287a6fdd1df97
Daniel Bitsch <bitsch@leadingsystems.de>
2022-01-20 09:22:40 +0100

    adding config.php

--- timeline/config.php
+++ timeline/config.php
@@ -10,6 +10,112 @@ $arr_config = [
     ],
     'standardFields' => ['cssID'],
     'fields' => [
+        'imageGroup' => [
+            'label' => ['Bildgröße'],
+            'inputType' => 'group'
+        ],
 
+        'size' => [
+            'label' => ['Bildbreite und Bildhöhe', ''],
+            'inputType' => 'imageSize',
+            'reference' => &$GLOBALS['TL_LANG']['MSC'],
+            'eval' => [
+                'rgxp' => 'digit',
+                'includeBlankOption' => true,
+                'tl_class' => 'w50'
+            ],
+            'options' => \System::getImageSizes()
+        ],
+
+        'boxes' => [
+            'label' => ['Zeiteinträge'],
+            'elementLabel' => '%s. Zeiteintrag',
+            'inputType' => 'list',
+            'fields' => [
+                'direction' => [
+                    'label' => ['Ausrichtung'],
+                    'inputType' => 'select',
+                    'options' => [
+                        'l' => 'Links',
+                        'r' => 'Rechts'
+                    ],
+                    'eval' => [
+                        'tl_class' => 'w50'
+                    ]
+                ],
+
+                'time' => [
+                    'label' => ['Zeitraum'],
+                    'inputType' => 'text',
+                    'eval' => [
+                        'tl_class' => 'w50 clr'
+                    ]
+                ],
+
+                'text' => [
+                    'label' => ['Text'],
+                    'inputType' => 'textarea',
+                    'eval'  => [
+                        'rte'=>'tinyMCE',
+                        'style' => 'height: 200px',
+                        'tl_class' => 'clr'
+                    ]
+                ],
+
+                'image' => [
+                    'label' => ['Bildauswahl'],
+                    'inputType' => 'fileTree',
+                    'eval' => [
+                        'fieldType' => 'radio',
+                        'filesOnly' => true,
+                        'extensions' => Contao\Config::get('validImageTypes'),
+                        'tl_class' => 'w50'
+                    ]
+                ],
+
+                'hyperlinkBoxes' => [
+                    'label' => ['Buttons'],
+                    'elementLabel' => '%s. Button',
+                    'inputType' => 'list',
+                    'fields' => [
+                        'hyperlinkHref' => [
+                            'label' => ['Button-Verlinkung'],
+                            'inputType' => 'url',
+                            'eval' => [
+                                'tl_class' => 'w50'
+                            ]
+                        ],
+                        'hyperlinkNewWindow' => [
+                            'label' => $GLOBALS['TL_LANG']['MSC']['target'],
+                            'inputType' => 'checkbox',
+                            'eval' => [
+                                'tl_class' => 'w50 cbx m12'
+                            ]
+                        ],
+                        'hyperlinkText' => [
+                            'label' => ['Button-Beschriftung'],
+                            'inputType' => 'text',
+                            'eval' => [
+                                'tl_class' => 'w50 clr'
+                            ]
+                        ],
+                        'hyperlinkTitle' => [
+                            'label' => ['Button-Titel'],
+                            'inputType' => 'text',
+                            'eval' => [
+                                'tl_class' => 'w50'
+                            ]
+                        ],
+                        'hyperlinkClass' => [
+                            'label' => ['Button-Klasse'],
+                            'inputType' => 'text',
+                            'eval' => [
+                                'tl_class' => 'w50 clr'
+                            ]
+                        ],
+                    ],
+                ],
+            ]
+        ]
     ]
 ];
