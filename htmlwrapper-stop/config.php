4d4beb50f0d05b1f860dbda234e52f69f989d9ce
Daniel Bitsch <bitsch@leadingsystems.de>
2024-01-16 11:44:30 +0100

    add HTML Wrapper

---
+++ htmlwrapper-stop/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$arr_config = [
+    'label' => ['HTML Umschlag Ende LSCE'],
+    'types' => ['content'],
+    'contentCategory' => 'texts',
+    'wrapper' => [
+        'type' => 'stop'
+    ],
+    'beTemplate' => 'rsce_htmlwrapper-stop',
+    'fields' => [
+        'html' => [
+            'inputType' => 'standardField'
+        ],
+    ]
+];
